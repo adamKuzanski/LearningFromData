b0b897d0173edd634a5a25b4b5d3edd473a4639e
Adam Kuzański <adamkuzanski@gmail.com>
2020-10-28 12:26:35 +0100

    Frontend displays movies and rating scale

--- frontend/film-recomendation-app/src/app/cinema-view/components/movie-list/movie-list.component.ts
+++ frontend/film-recomendation-app/src/app/cinema-view/components/movie-list/movie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn } from '@angular/forms';
 import { Movie } from '../../models/movie';
 import { MovieService } from '../../services/movie.service';
 
@@ -7,14 +8,46 @@ import { MovieService } from '../../services/movie.service';
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
+
 export class MovieListComponent implements OnInit {
-  constructor(private movieService: MovieService) { }
-  movies: Movie[] = [];
+  constructor(
+    private movieService: MovieService,
+    private formBuilder: FormBuilder) { }
+  
+  arrayMovies: Movie[] = [];
+
+  form: FormGroup;
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      demoArray: this.formBuilder.array([
+        this.formBuilder.control('')
+      ])
+    });
+
     this.movieService.getMovies().subscribe({
-      next: (movies) => this.movies = movies,
+      next: (movies) => {
+        this.arrayMovies = movies;
+        this.addMovies(movies);
+      },
       error: () => alert('Nie udało się pobrać filmów')
-    })
+    });
   }
+
+  get demoArray() {
+    return this.form.get('demoArray') as FormArray;
+  }
+
+  public addMovies(movies: Movie[]) {
+    movies.forEach(() => this.demoArray.push(this.formBuilder.control(false)));
+  }
+
+  public updateCheckboxes(): void {
+    this.arrayMovies.forEach(() => this.demoArray.push(this.formBuilder.control(false)) );
+  }
+
+  public submitForm(formValue) {
+    console.log(formValue);
+  }
+
 }
