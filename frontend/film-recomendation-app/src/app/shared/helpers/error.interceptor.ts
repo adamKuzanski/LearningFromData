ab10c315e09ff05c9106f80e4559427f7643d307
Adam Kuzański <adamkuzanski@gmail.com>
2020-11-12 04:07:53 +0100

    Frontend fix

--- frontend/film-recomendation-app/src/app/shared/helpers/error.interceptor.ts
+++ frontend/film-recomendation-app/src/app/shared/helpers/error.interceptor.ts
@@ -23,7 +23,8 @@ export class ErrorInterceptor implements HttpInterceptor {
         location.reload();
       }
 
-      const error = err.error.message || err.statusText;
+      // const error = err.error.message || err.statusText;
+      const error = "Something went wrong, try again";
       return throwError(error);
     }));
   }
