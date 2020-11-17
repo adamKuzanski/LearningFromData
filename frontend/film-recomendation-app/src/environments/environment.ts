54d062b23e243affcbe1af4083a8d5a0785c1703
Adam <adamkuzanski@gmail.com>
2020-11-17 02:24:00 +0100

    Small fix

--- frontend/film-recomendation-app/src/environments/environment.ts
+++ frontend/film-recomendation-app/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44379/api'
+  // apiUrl: 'https://localhost:44379/api'
+  apiUrl: 'https://dotnetmoviesbackend.azurewebsites.net/api'
 };
 
 /*
