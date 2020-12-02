2f16f9d9aed270c4ed959ee831407356d54b6c06
Adam <adamkuzanski@gmail.com>
2020-12-02 12:43:38 +0100

    Some clearance

--- frontend/film-recomendation-app/src/app/cinema-view/cinema-view-routing.module.ts
+++ frontend/film-recomendation-app/src/app/cinema-view/cinema-view-routing.module.ts
@@ -1,6 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DynamicFormQuestionComponent } from './components/dynamic-form-question/dynamic-form-question.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { MainSimpleComponent } from './components/main-simple/main-simple.component';
 import { RecommendationMainComponent } from './components/recommendation-main/recommendation-main.component';
--- frontend/film-recomendation-app/src/app/cinema-view/cinema-view.module.ts
+++ frontend/film-recomendation-app/src/app/cinema-view/cinema-view.module.ts
@@ -6,8 +6,6 @@ import { MainLayoutComponent } from './components/main-layout/main-layout.compon
 import { MovieComponent } from './components/movie/movie.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DynamicFormQuestionComponent } from './components/dynamic-form-question/dynamic-form-question.component';
-import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 import { RecommendationMainComponent } from './components/recommendation-main/recommendation-main.component';
 import { MainSimpleComponent } from './components/main-simple/main-simple.component';
 import { MovieSimpleComponent } from './components/movie-simple/movie-simple.component';
@@ -23,8 +21,6 @@ import { MovieSimpleComponent } from './components/movie-simple/movie-simple.com
     MainLayoutComponent,
     MovieComponent,
     MovieListComponent,
-    DynamicFormQuestionComponent,
-    DynamicFormComponent,
     RecommendationMainComponent,
     MainSimpleComponent,
     MovieSimpleComponent
